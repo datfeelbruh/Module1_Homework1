c79c176de0bb36b15320dfb9bd5c028b35a2ab8b
khoroshko.d <datfeelman@gmail.com>
2022-07-27 19:24:41 +0300

    fix

--- src/Homework3/Expert/Homework3Expert.java
+++ src/Homework3/Expert/Homework3Expert.java
@@ -1,13 +1,10 @@
 package Homework3.Expert;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
